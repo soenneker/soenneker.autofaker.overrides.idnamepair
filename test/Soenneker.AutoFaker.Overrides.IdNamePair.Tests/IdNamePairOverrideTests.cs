45ee32812b8d1e3d82b013b3afe8d923e1c12ea0
Jake Soenneker <jake@soeneker.com>
2024-12-23 17:53:16 +0000

    Tests

--- test/Soenneker.AutoFaker.Overrides.IdNamePair.Tests/IdNamePairOverrideTests.cs
+++ test/Soenneker.AutoFaker.Overrides.IdNamePair.Tests/IdNamePairOverrideTests.cs
@@ -1,5 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
 namespace Soenneker.AutoFaker.Overrides.IdNamePair.Tests;
 
-public class IdNamePairOverrideTests
+[Collection("Collection")]
+public class IdNamePairOverrideTests : FixturedUnitTest
 {
+    public IdNamePairOverrideTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
 }
